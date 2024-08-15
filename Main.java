0453f4756929ddf165f453b178b0584d75507a42
KuldipMahale <kuldipmahale15@gmail.com>
2024-08-15 20:12:14 +0530

    Update Main.java
    new line added to test CICD pipeline

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
         System.out.println("Hello How are you");
         System.out.println("This is first program with jenkins");
         System.out.println(new Date());
+        System.out.println("This is new line added to test Pipeline");
 
     }
 
