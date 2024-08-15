56545ed4fb6852b41764b2f62a2158422be05056
KuldipMahale <kuldipmahale15@gmail.com>
2024-08-15 20:05:36 +0530

    Update Main.java
    Removed Additional J from code

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.util.Date;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello How are you");
+        System.out.println("This is first program with jenkins");
         System.out.println(new Date());
 
     }
 
-}j
+}
